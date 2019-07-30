8b65e5c7d257da28aa29e3c5262a1f7272ba569f
Tristan Smith <tristan@shortcord.com>
2019-07-29 21:16:05 -0400

    added comments for BuildInfo.cs

--- src/BuildInfo.cs
+++ src/BuildInfo.cs
@@ -2,14 +2,26 @@
 {
     public struct BuildInfo
     {
+        /// <summary>
+        /// The Build number for the loaded datafiles.
+        /// </summary>
         public string BuildNumber { get; internal set; }
+        /// <summary>
+        /// The date that the loaded datafiles were built.
+        /// </summary>
         public string DataBaseFileDate { get; internal set; }
+        /// <summary>
+        /// The date that the loaded datafiles will exipre, this does not account for the license lenght.
+        /// </summary>
         public string DatabaseExpirationDate { get; internal set; }
+        /// <summary>
+        /// The date that the loaded RBDI database was build.
+        /// </summary>
         public string RBDIDatabaseDate { get; internal set; }
 
         public override string ToString()
         {
-            return $"Build Number: {BuildNumber} | Built: {DataBaseFileDate}; Expiration: {DatabaseExpirationDate}";
+            return $"Build Number: {BuildNumber} | Built: {DataBaseFileDate}; Expiration: {DatabaseExpirationDate} | RBDI Date {RBDIDatabaseDate}";
         }
     }
 }
